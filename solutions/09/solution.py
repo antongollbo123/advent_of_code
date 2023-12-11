3df9c46acde83c1fb97269b89b4dddf5598a5306
antongollbo123 <antongollbo@gmail.com>
2023-12-11 22:13:09 +0100

    Part 2

--- solutions/09/solution.py
+++ solutions/09/solution.py
@@ -21,10 +21,22 @@ def part_2(text_content:str) -> int:
         int_num_list = [int(ele) for ele in i.split()]
         number_lists.append(int_num_list)
     tot_sum = 0
+    
+    num_dict = {}
+    for i, number_list in enumerate(number_lists):
+        number_list.reverse()
+        new_number_list = []
+        difference = [-1]
+        new_number_list.append(number_list)
+        while any (diff!= 0 for diff in difference):
+            tot_sum += number_list[-1]
+            difference = [number_list[i+1] - number_list[i] for i in range(len(number_list)-1)]
+            number_list = difference
+            new_number_list.append(number_list)
     return tot_sum
 
 if __name__ == "__main__":
-    filename = "sample_input.txt"
+    filename = "real_input.txt"
     with open(filename, "r") as file:
         content = file.read()
         output = part_1(text_content = content)
